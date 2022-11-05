bd6fefb40e012ff8f1918bbe8d920c391721c9e5
aps <abhishekpratap.singh@databricks.com>
2022-11-05 14:42:56 +1100

    Refactoring

--- model/DatabricksClient.py
+++ model/DatabricksClient.py
@@ -17,6 +17,8 @@ class DatabricksClient:
         response = requests.get(api_url,  headers=my_headers).text
         return json.loads(response)
 
+
+
     def createdbuser(self,user):
         api_url = self.dbbaseUrl+"/Users"
         u={
@@ -41,8 +43,6 @@ class DatabricksClient:
               ]
             }
 
-
-        my_dict = {"Name": [], "Address": [], "Age": []};
         mem =[]
         for member in members:
             for dbu in dbus["Resources"]:
@@ -51,12 +51,6 @@ class DatabricksClient:
                     obj["value"] = dbu["id"]
                     mem.append(obj)
 
-
-
-
-
-
-
         gdata=json.loads(json.dumps(u))
         # for member in members:
         #     gdata["members"].append(dict("value",member[0]))
@@ -64,6 +58,80 @@ class DatabricksClient:
         ujson=json.dumps(gdata)
         my_headers = {'Authorization': 'Bearer '+self.dbscimToken}
         response = requests.post(api_url, data=ujson, headers=my_headers)
+        print(ujson)
+
+    def patchdbgroup(self, gid, members, dbg, dbus):
+        api_url = self.dbbaseUrl + "/Groups/"+gid
+        u = {
+            "schemas": [
+                "urn:ietf:params:scim:schemas:core:2.0:Group"
+            ]
+        }
+
+        toadd = []
+        toremove = []
+
+        for member in members:
+            exists = False
+            #if dbg["members"] exists:
+            if "members" in dbg :
+                for dbmember in dbg["members"]:
+                    if member[0].casefold() == dbmember["display"].casefold():
+                        exists = True;
+            if not exists :
+                toadd.append(member)
+
+        if "members" in dbg:
+            for dbmember in dbg["members"]:
+                exists = False
+                for member in members:
+                    if member[0].casefold() == dbmember["display"].casefold():
+                        exists = True;
+                if not exists :
+                    toremove.append(member)
+
+        ops = []
+
+        if len(toadd) ==0 and len(toremove)==0:
+            return
+
+        if len(toadd) > 0:
+            mem = []
+            for member in toadd:
+                for dbu in dbus["Resources"]:
+                    if dbu["displayName"] == member[0]:
+                        obj = dict()
+                        obj["value"] = dbu["id"]
+                        mem.append(obj)
+
+            #add
+            dictmem = {"members": mem}
+            dictsub = {'op': "add", 'path': "members", 'value': dictmem}
+            ops.append(dictsub)
+
+        if len(toremove) > 0:
+            mem = []
+            for member in toremove:
+                for dbu in dbus["Resources"]:
+                    if dbu["displayName"] == member[0]:
+                        obj = dict()
+                        obj["value"] = dbu["id"]
+                        mem.append(obj)
+
+            dictmem = {"members": mem}
+            dictsub = {'op': "add", 'path': "members", 'value': dictmem}
+            ops.append(dictsub)
+
+
+
+
+        gdata = json.loads(json.dumps(u))
+        # for member in members:
+        #     gdata["members"].append(dict("value",member[0]))
+        gdata["Operations"] = ops
+        ujson = json.dumps(gdata)
+        my_headers = {'Authorization': 'Bearer ' + self.dbscimToken}
+        response = requests.patch(api_url, data=ujson, headers=my_headers)
         print(ujson)
 
     def get_DBGroups(self):
@@ -86,4 +154,6 @@ class DatabricksClient:
         my_headers = {'Authorization': 'Bearer ' + self.dbscimToken}
         response = requests.delete(api_url, headers=my_headers).text
         return response
+
+
 
--- notebooks/main.py
+++ notebooks/main.py
@@ -1,5 +1,3 @@
-
-
 import sys
 
 import configparser
@@ -8,15 +6,8 @@ from model.DatabricksClient import DatabricksClient
 from model.Graph import Graph
 from collections import defaultdict
 
-
-
 groupUsermap = defaultdict(set)
 userGroupmap = defaultdict(set)
-
-
-
-
-
 
 
 def main():
@@ -25,9 +16,22 @@ def main():
     print('Number of arguments:', len(sys.argv[1:]), 'arguments.')
     print('Argument List:', str(sys.argv[1:]))
 
-    for arg in str(sys.argv[1:]):
-        if arg.casefold()=="--dryRun":
+    if len(sys.argv[1:]) > 2:
+        print("only 2 arguments supported")
+        return
+
+    toplevelgroup = ""
+
+    for arg in sys.argv[1:]:
+        if arg.casefold() == "--dryRun":
             dryrun = True;
+        else:
+            if toplevelgroup == "":
+                toplevelgroup = arg;
+            else:
+                print("Only one group supported")
+                return
+
 
     # Load settings
     config = configparser.ConfigParser()
@@ -36,49 +40,55 @@ def main():
     db_settings = config['databricks']
 
     graph: Graph = Graph(azure_settings)
-    dbclient : DatabricksClient = DatabricksClient(db_settings)
+    dbclient: DatabricksClient = DatabricksClient(db_settings)
 
-    dbusers=dbclient.get_DBUsers()
+    dbusers = dbclient.get_DBUsers()
 
-    users_page = graph.get_users()
-
-    print("1.All Users Read")
+    print("1.All Databricks Users Read")
 
     groups_page = graph.get_groups()
 
-    print("2.All top level groups Read")
+    print("2.All top level AAD groups Read")
+    print("3.Top level group requested is " + toplevelgroup)
 
     for group in groups_page['value']:
-        print("Group is "+group["displayName"])
+        print("Group is " + group["displayName"])
         for arg in sys.argv[1:]:
             if not arg.startswith("--") and arg.casefold() == group["displayName"].casefold():
-                groupUsermapU, userGroupmapU=graph.extractFromGroup(graph, group["id"], group["displayName"], groupUsermap, userGroupmap);
+                groupUsermapU, userGroupmapU = graph.extractFromGroup(graph, group["id"], group["displayName"],
+                                                                      groupUsermap, userGroupmap);
 
+    print("4.Hierarchy analysed,going to create users and groups")
 
     for u in userGroupmapU.keys():
         if not dryrun:
             exists = False
 
             for udb in dbusers["Resources"]:
-                if u[0].casefold() == udb["displayName"].casefold() :
+                if u[0].casefold() == udb["displayName"].casefold():
                     exists = True;
 
             if not exists:
+
                 dbclient.createdbuser(u)
+                print("User created in databricks : " + u)
 
     dbusers = dbclient.get_DBUsers()
+    dbGroups = dbclient.get_DBGroups()
 
     for u in groupUsermapU.keys():
-        print("2")
-        dbclient.createdbgroup(u,groupUsermapU.get(u),dbusers)
+        exists = False
+        for dbg in dbGroups["Resources"]:
+            if u.casefold() == dbg["displayName"].casefold():
+                exists = True;
+                #compare and add remove the members as needed
+                dbclient.patchdbgroup(dbg["id"], groupUsermap.get(u), dbg,dbusers)
 
 
-
-
-
-
+        if not exists:
+            dbclient.createdbgroup(u, groupUsermap.get(u), dbusers)
+            print("Group and User assignment created in databricks : " + u)
 
 
 if __name__ == '__main__':
-
     main()
