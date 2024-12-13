1d25dda6e8dd25d4f19396c0b55454ce09a94c9c
aps <abhishekpratap.singh@databricks.com>
2024-12-13 14:05:19 +1100

    Bumping version for Pypi Setting up requirements version

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="nestedaaddb",
+    version="1.0.0",
+    packages=find_packages(where="nestedaaddb"),
+    package_dir={"": "nestedaaddb"},
+    include_package_data=True,
+)
